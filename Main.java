acdebae94959e747e20e5bc6698b3f1eaaf03c11
ASHCASH23 <ashishjonna808@gmail.com>
2026-07-07 21:54:11 -0400

    Add Main class for store application entry point

---
+++ Main.java
@@ -0,0 +1,10 @@
+package mystore; 
+
+public class Main { 
+    public static void main (String[] args) { 
+        NewStore mystore = new NewStore(); 
+        
+        mystore.printString("Nike"); // object - instance of a class - specific  name
+        // class - blueprint - general term
+    } 
+}
