fc8612a722027719f5b7cb0c1d8f0f7db7efb2ac
ASHCASH23 <ashishjonna808@gmail.com>
2026-07-07 22:40:22 -0400

    Create NewStore class with print and store methods

---
+++ NewStore2.java
@@ -0,0 +1,15 @@
+package mystore;
+
+public class NewStore {
+	
+     
+	 
+    public void printString(String myStore) { 
+        System.out.println("Store " + myStore);
+    }
+    
+    public int store(int numstores, int numstores1) 
+    {
+    	return numstores *= numstores1; 
+    }
+}
